963aa4461d2fd444885770924f7c2104ec55a914
William Norman <william@abios.se>
2023-09-27 08:28:08 +0200

    removed all selection_intensity related stuff

--- evographs_core/moranmodel.py
+++ evographs_core/moranmodel.py
@@ -3,44 +3,37 @@ from graph import Graph
 from tqdm import tqdm
 
 
-def moran_model_simulation(graph: Graph, generations: int, selection_intensity: float):
+def moran_model_simulation(graph: Graph, generations: int):
     """
     Simulate the (spatial) Moran model on a graph for a specified number of generations.
 
     Parameters:
         graph: The initial population graph.
         generations: The number of generations to simulate.
-        selection_intensity: The selection intensity parameter.
         mutation_rate: The mutation rate parameter.
 
     Returns:
         A list of Graph objects representing the state of the population at each generation.
     """
-    if not (0 <= selection_intensity <= 1):
-        raise ValueError(
-            f"Selection intensity: {selection_intensity} is outside the range [0, 1]"
-        )
-
     population_history = [graph.copy()]
     for _ in tqdm(range(generations)):
-        next_generation = reproduce_population(graph.copy(), selection_intensity)
+        next_generation = reproduce_population(graph.copy())
         population_history.append(next_generation)
     return population_history
 
 
-def reproduce_population(graph: Graph, selection_intensity: float):
+def reproduce_population(graph: Graph):
     """
     Reproduce the population on the graph based on selection.
 
     Parameters:
         graph: The current population graph.
-        selection_intensity: The selection intensity parameter.
 
     Returns:
         The population graph representing the next generation.
     """
     next_generation = graph.copy()
-    selected_node = select_individual(graph, selection_intensity)
+    selected_node = select_individual(graph)
 
     # choose neighbor uniformly who should inherit the selected node's genotype
     neighbor_candidates = list(graph.nodes[selected_node])
@@ -54,7 +47,7 @@ def reproduce_population(graph: Graph, selection_intensity: float):
     return next_generation
 
 
-def calculate_fitness(genotype: str, selection_intensity: float):
+def calculate_fitness(genotype: str):
     """
     Calculate the fitness of an individual based on its genotype.
 
@@ -64,11 +57,10 @@ def calculate_fitness(genotype: str, selection_intensity: float):
     Returns:
         float: The fitness value.
     """
-    # TODO: Add proper fitness function based on match-up outcomes between genotypes
     return len(genotype)
 
 
-def select_individual(graph: Graph, selection_intensity: float):
+def select_individual(graph: Graph):
     """
     Select an individual probabilistically (probabilities proportional to fitness) for reproduction.
 
@@ -78,9 +70,7 @@ def select_individual(graph: Graph, selection_intensity: float):
     Returns:
         Node: The selected individual for reproduction.
     """
-    fitness_values = [
-        calculate_fitness(node.genotype, selection_intensity) for node in graph.nodes
-    ]
+    fitness_values = [calculate_fitness(node.genotype) for node in graph.nodes]
     total_fitness = sum(fitness_values)
     selection_probabilities = [fitness / total_fitness for fitness in fitness_values]
     selected_node = random.choices(list(graph.nodes), selection_probabilities)[0]
