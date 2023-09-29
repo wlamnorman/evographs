da297067597096621656d6a216218bd3f9f7ed29
William Norman <william@abios.se>
2023-09-29 22:55:30 +0200

    added plot_graph which plots a Graph class object using conversion to NetworkX class object

--- evographs_core/graph/visualisation.py
+++ evographs_core/graph/visualisation.py
@@ -3,7 +3,66 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def convert_to_networkx(graph: Graph) -> tuple[nx.Graph, dict]:
+def plot_graph(
+    graph: Graph,
+    node_size: int = 750,
+    genotype_label_font_size: int = 10,
+    id_label_font_size: int = 6,
+):
+    """
+    Plots an undirected graph using NetworkX with customizable node and label attributes.
+
+    Args:
+        graph: The Graph object to be plotted.
+        node_size: The size of the nodes in the graph plot. Defaults to 750.
+        genotype_label_font_size: Font size for genotype labels. Defaults to 10.
+        id_label_font_size: Font size for ID labels. Defaults to 6.
+
+    Returns:
+        None
+
+    This function uses NetworkX to create a graphical representation of an undirected graph.
+    It visualizes the nodes with customizable node size and color based on genotypes.
+    Genotype and ID labels are added to the nodes with customizable font sizes.
+    The resulting plot is displayed using Matplotlib.
+    """
+    nx_graph = convert_to_networkx(graph)
+
+    genotype_colors = generate_genotype_colors(graph.genotype_valuecounts.keys())
+    genotypes = [nx_graph.nodes[node]["genotype"] for node in nx_graph.nodes]
+    node_colors = [genotype_colors[genotype] for genotype in genotypes]
+
+    pos = nx.circular_layout(nx_graph)  # type: ignore
+    nx.draw(  # type: ignore
+        nx_graph, pos, with_labels=False, node_color=node_colors, node_size=node_size
+    )
+
+    genotype_labels = {
+        node: f"{nx_graph.nodes[node]['genotype']}" for node in nx_graph.nodes
+    }
+
+    # Add genotype labels
+    nx.draw_networkx_labels(  # type: ignore
+        nx_graph,
+        pos,
+        labels=genotype_labels,
+        font_size=genotype_label_font_size,
+        verticalalignment="bottom",
+    )
+
+    # Add ID labels with smaller font size below genotype labels
+    id_labels = {node: f"{node}" for node in nx_graph.nodes}
+    nx.draw_networkx_labels(  # type: ignore
+        nx_graph,
+        pos,
+        labels=id_labels,
+        font_size=id_label_font_size,
+        verticalalignment="top",
+    )
+    plt.show()
+
+
+def convert_to_networkx(graph: Graph) -> nx.Graph:
     """
     Converts a Graph to a NetworkX Graph.
 
@@ -14,19 +73,16 @@ def convert_to_networkx(graph: Graph) -> tuple[nx.Graph, dict]:
         nx.Graph: The equivalent NetworkX Graph object.
     """
     nx_graph = nx.Graph()
-    node_labels = {}
-
     for node in graph.nodes:
         node_id = node.node_id
         genotype = node.genotype
         nx_graph.add_node(node_id, genotype=genotype)
-        node_labels[node_id] = f"{genotype}\n{node_id}"
 
     for node, neighbors in graph.nodes.items():
         for neighbor in neighbors:
             nx_graph.add_edge(node.node_id, neighbor.node_id)
 
-    return nx_graph, node_labels
+    return nx_graph
 
 
 def generate_genotype_colors(genotypes):
